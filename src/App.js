13f899505cd2d16c1ce7a423cac0a76ce02c5385
Morgan West <mewwes@gmail.com>
2023-05-14 23:10:37 -0700

    body build stock track initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import StockContainer from './containers/StockContainer'
 
-function App() {
+const stocks = ['GOOG', 'AAPL', 'FB', 'AMZN', 'NFLX']
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Stock Tracker</h1>
+        <div class="stocks">
+            {stocks.map(stock => <StockContainer key={stock} stock={stock} />)}
+        </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
